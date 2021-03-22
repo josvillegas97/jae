370a819521030f8a42f59ab760de5eafd1951748
josvillegas <josvillegas@upeu.edu.pe>
2021-03-21 20:40:51 -0500

    first

---
+++ js/carrusel.js
@@ -0,0 +1,33 @@
+
+
+
+
+$(document).ready(function() {
+    $('#autoWidth').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function() {
+            $('#autoWidth').removeClass('cS-hidden');
+        } 
+    }); 
+
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 2500) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $('#back-to-top').click(function () {
+        $('#back-to-top').tooltip('hide');
+        $('body,html').animate({
+            scrollTop: 0
+        }, 900);
+        return false;
+    });
+    
+
+
+  });
